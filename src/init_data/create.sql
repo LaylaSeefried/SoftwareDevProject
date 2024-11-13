f894573bd3d1b047b553c44375faf29bdc81c391
nathanmahary <nama8858@colorado.edu>
2024-11-12 18:30:45 -0700

    Updated Login

--- src/init_data/create.sql
+++ src/init_data/create.sql
@@ -18,7 +18,7 @@ CREATE TABLE courses (
 DROP TABLE IF EXISTS student_courses;
 CREATE TABLE user_courses (
   course_id INTEGER NOT NULL REFERENCES courses (course_id),
-  username INTEGER NOT NULL REFERENCES users (username),
-  PRIMARY KEY (course_id, student_id)
+  student_id INTEGER NOT NULL REFERENCES students (student_id),
+  RIMARY KEY (course_id, student_id)
 );
 
