64fef32a9744b0945bc990e9d9af1510c7c6ba77
MatusalaT <mate7043@colorado.edu>
2024-11-13 18:38:25 -0700

    Minor change to database

--- src/init_data/create.sql
+++ src/init_data/create.sql
@@ -18,7 +18,7 @@ CREATE TABLE courses (
 DROP TABLE IF EXISTS student_courses;
 CREATE TABLE user_courses (
   course_id INTEGER NOT NULL REFERENCES courses (course_id),
-  student_id INTEGER NOT NULL REFERENCES students (student_id),
-  RIMARY KEY (course_id, student_id)
+  username VARCHAR(50) NOT NULL REFERENCES users (username),
+  PRIMARY KEY (course_id, username)
 );
 
