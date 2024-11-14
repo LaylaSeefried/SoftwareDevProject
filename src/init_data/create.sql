d439922d258cf18417516dd07786a9cce4afdb41
MatusalaT <mate7043@colorado.edu>
2024-11-13 19:18:13 -0700

    Somewhat working database

--- src/init_data/create.sql
+++ src/init_data/create.sql
@@ -16,9 +16,8 @@ CREATE TABLE courses (
 );
 
 DROP TABLE IF EXISTS student_courses;
-CREATE TABLE user_courses (
+CREATE TABLE student_courses (
   course_id INTEGER NOT NULL REFERENCES courses (course_id),
   username VARCHAR(50) NOT NULL REFERENCES users (username),
   PRIMARY KEY (course_id, username)
 );
-
