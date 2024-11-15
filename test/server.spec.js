83183dc9adaef46fcb49bb7cef4059a82a9c9231
nathanmahary <nama8858@colorado.edu>
2024-11-15 10:21:46 -0700

    Fixed Profile

--- test/server.spec.js
+++ test/server.spec.js
@@ -1,7 +1,6 @@
-
 // // ********************** Initialize server **********************************
 
-// const server = require('../index.js'); //TODO: Make sure the path to your index.js is correctly added
+// const server = require('../src/index.js'); //TODO: Make sure the path to your index.js is correctly added
 
 // // ********************** Import Libraries ***********************************
 
@@ -39,6 +38,27 @@
 // // Result: This test case should pass and return a status 200 along with a "Success" message.
 // // Explanation: The testcase will call the /add_user API with the following input
 // // and expects the API to return a status of 200 along with the "Success" message.
+// // Example Positive Testcase :
+// // API: /add_user
+// // Input: {id: 5, name: 'John Doe', dob: '2020-02-20'}
+// // Expect: res.status == 200 and res.body.message == 'Success'
+// // Result: This test case should pass and return a status 200 along with a "Success" message.
+// // Explanation: The testcase will call the /add_user API with the following input
+// // and expects the API to return a status of 200 along with the "Success" message.
+
+// describe('Testing Add User API', () => {
+//   it('positive : /register', done => {
+//     chai
+//       .request(server)
+//       .post('/register')
+//       .send({id: 12, username: 'Yo yo', password: "123", bio: 'EE', year: 'junior', major: "EE"})
+//       .end((err, res) => {
+//         expect(res).to.have.status(200);
+//         expect(res.body.message).to.equals('Success');
+//         done();
+//       });
+//   });
+// });
 
 // describe('Testing Add User API', () => {
 //   it('positive : /add_user', done => {
@@ -73,3 +93,53 @@
 //       });
 //   });
 
+// // Example Negative Testcase :
+//   // API: /add_user
+//   // Input: {id: 5, name: 10, dob: '2020-02-20'}
+//   // Expect: res.status == 400 and res.body.message == 'Invalid input'
+//   // Result: This test case should pass and return a status 400 along with a "Invalid input" message.
+//   // Explanation: The testcase will call the /add_user API with the following invalid inputs
+//   // and expects the API to return a status of 400 along with the "Invalid input" message.
+//   it('Negative : /register. Checking invalid name', done => {
+//     chai
+//       .request(server)
+//       .post('/register')
+//       .send({id: 12, username: 'Yo yo', password: "123", bio: 'EE', year: 'junior', major: "EE"})
+//       .end((err, res) => {
+//         expect(res).to.have.status(400);
+//         expect(res.body.message).to.equals('Invalid input');
+//         done();
+//       });
+//   });
+
+
+// //postive test #2
+// describe('Testing Login API', () => {
+//   it('positive: /login - correct credentials', done => {
+
+//     chai
+//       .request(server)
+//       .post('/login')
+//       .send({ username: 'johndoe', password: 'password1' })
+//       .end((err, res) => {
+//         expect(res).to.have.status(302); 
+//         expect(res).to.redirectTo('/home'); 
+
+//         done();
+//       });
+//   });
+// });
+
+//   it('negative: /login - non-existent username', done => {
+//     chai
+//       .request(server)
+//       .post('/login')
+//       .send({ username: 'NonExistentUser', password: '123' })
+//       .end((err, res) => {
+//         // Check for redirect to login page when user is not found
+//         expect(res).to.have.status(302); // Status 302 means redirect
+//         expect(res).to.redirectTo('/login'); // Expect redirect back to login
+
+//         done();
+//       });
+//   });
