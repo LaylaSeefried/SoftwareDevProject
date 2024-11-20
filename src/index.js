2fdf69ccd60515cead5475ca7008dda150ab41da
Layla Seefried <lase4182@colorado.edu>
2024-11-19 17:35:49 -0700

    searchbar functioning

--- src/index.js
+++ src/index.js
@@ -55,8 +55,8 @@ app.set('view engine', 'hbs');
 app.set('views', path.join(__dirname, 'views'));
 app.use(bodyParser.json()); // specify the usage of JSON for parsing request body.
 
-// Serve static files from the 'resources' directory
-app.use(express.static(path.join(__dirname, 'src/resources')));
+app.use('/css', express.static(path.join(__dirname, 'resources/css')));
+app.use('/js', express.static(path.join(__dirname, 'resources/js')));
 
 // initialize session variables
 app.use(
